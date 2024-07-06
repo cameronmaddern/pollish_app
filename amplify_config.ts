212c5e05b797ac1eb3513655bcc4b0594ba92fae
profferamaster <128373717+profferamaster@users.noreply.github.com>
2024-07-07 00:06:35 +1000

    Update amplify_config.ts

--- amplify_config.ts
+++ amplify_config.ts
@@ -1,17 +1,17 @@
 import {
   REACT_APP_API_KEY,
-  REACT_APP_USER_POOL_ID,
-  REACT_APP_USER_POOL_WEB_CLIENT_ID,
-  REACT_APP_IDENTITY_POOL_ID,
+  REACT_APP_APPSYNC_AUTH_TYPE,
   REACT_APP_APPSYNC_GRAPHQL_ENDPOINT,
   REACT_APP_APPSYNC_REGION,
-  REACT_APP_APPSYNC_AUTH_TYPE,
+  REACT_APP_IDENTITY_POOL_ID,
+  REACT_APP_USER_POOL_ID,
+  REACT_APP_USER_POOL_WEB_CLIENT_ID,
 } from "@env";
 
 const amplifyConfig = {
   Auth: {
     Cognito: {
-      userPoolId: "ap-southeast-2_HlGnN9Vxh",
+      userPoolId: REACT_APP_USER_POOL_ID as string,
       identityPoolId: REACT_APP_IDENTITY_POOL_ID as string,
       userPoolClientId: REACT_APP_USER_POOL_WEB_CLIENT_ID as string,
     },
